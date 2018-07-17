6b2f9334676f5c300151f40fa11a42911189dafa
ars33 <ars33@MacBook-Pro-ars33.local>
2018-07-17 12:17:43 +0300

    test

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .headerTop
+    .block.logo
+      img(src="http://placehold.it/120x120")
+    .mblock
+      span +7 977 47 192 47
+      span hello@lilamp.com
+    .block
+      span +7 977 47 192 47
+    .block
+      span hello@lilamp.com
+    .block(style="flex: 2")
+       a доставка и оплата
+       a о нас
+</template>
+
+<style>
+
+</style>
