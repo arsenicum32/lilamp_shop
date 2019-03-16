d39b88dc1b7d538ba2cf0c5b716497c425339dff
arseniy <apopov@sellbery.com>
2019-03-17 02:37:06 +0300

    deploy with autodeploy

---
+++ _nuxt/pages/product/index.9bdf3daa46d81678c5dd.js
@@ -0,0 +1,1 @@
+webpackJsonp([22],{"1Rnn":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=n("MGIw"),s=n("VU/8")(null,r.a,!1,null,null,null);t.default=s.exports},MGIw:function(e,t,n){"use strict";var r={render:function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("test")])},staticRenderFns:[]};t.a=r}});
